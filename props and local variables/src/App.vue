37cdebe96e3e702abeaa8d8582430302fc28017c
Morgan <ycshinichi@163.com>
2026-02-27 12:21:25 +0800

    Add props and local variables section

---
+++ props and local variables/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class ="rounded  d-inline-block bg-info p-2 m-2 ">
+    <h1>{{ msg }}</h1>
+    <Contact name=" Jhson" phone = "123-456-7890" ownername ="abs"/>
+    <ButtonCounter />
+    
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';     
+  import ButtonCounter from './components/ButtonCounter.vue'
+  import Contact from './components/Contact.vue'
+  const msg = 'Hello Vue!'
+</script>
---
+++ props and local variables/src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+        <div >
+                <h2>Contact Us</h2>
+                <p>Name : {{ name }}</p>
+                <p>Email : {{ email }}.</p>
+                <p>Phone : {{ phone }}</p>
+                <p v-if="ownername != ''">Owner Name : {{ ownername }}</p>
+        </div>
+</template>
+
+<script setup>
+        import { defineProps } from 'vue'
+        const { name, phone, ownername, email } = defineProps({
+                name: { type: String, required: true },
+                phone: { type: String },
+                ownername: { type: String },
+                email: { type: String, default: 'n/a' },
+        })
+</script>
