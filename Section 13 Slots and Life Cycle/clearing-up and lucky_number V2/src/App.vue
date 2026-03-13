6443488c2411d01be2e8f2af38620e1fae1e4452
Morgan <ycshinichi@163.com>
2026-03-13 08:51:40 +0800

    cleaning-up and luckyNumberV2

---
+++ Section 13 Slots and Life Cycle/clearing-up and lucky_number V2/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container bg-black text pt-3" :style="{height: '100vh'}">
+    <h1 class="text-center text-success">ContactOpedia</h1>
+
+    <div class="row align-items-center">
+      <LuckyNumber />
+      <LuckyNumberV2 />
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { ref, reactive } from 'vue';
+import LuckyNumber from './components/LuckyNumber.vue';
+import LuckyNumberV2 from './components/LuckyNumberV2.vue';
+
+
+
+
+
+</script>
