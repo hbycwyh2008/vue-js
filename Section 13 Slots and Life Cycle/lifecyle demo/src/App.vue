bc92fcc75e21b99cc2eaa3874ec1189885495b13
Morgan <ycshinichi@163.com>
2026-03-17 13:12:45 +0800

    lifecyle demo

---
+++ Section 13 Slots and Life Cycle/lifecyle demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="showMessage">Show Message</button>
+  <br/>
+  <h1>{{ count }}</h1>
+  <button @click="count++">Count: {{ count }}</button>
+  <button v-if="count==1"></button>
+</template>
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue';
+import {ref,onBeforeMount, onMounted, onBeforeUnmount, onUnmounted, ref} from 'vue';
+const message = ref('Hello Vue');
+const showMessage = () => {
+  message.value = 'Hello Vue from function!';
+};
+onBeforeMount(() => {
+  console.log('OnBeforeMount: app.vue');
+});
+const count = ref(0);
+onMounted(() => {
+  console.log('OnMounted: app.vue');
+});
+onBeforeUnmount(() => {
+  console.log('OnBeforeUnmount: app.vue');
+});
+onUnmounted(() => {
+  console.log('OnUnmounted: app.vue');
+}); 
+
+
+</script>
---
+++ Section 13 Slots and Life Cycle/lifecyle demo/src/components/ButtonCounter.vue
@@ -0,0 +1,22 @@
+<template>
+        <button @click="count++">Clicked {{ count }} times!</button>
+</template>
+
+<script setup>
+import {ref,onBeforeMount, onMounted, onBeforeUnmount, onUnmounted} from 'vue';
+onBeforeMount(() => {
+  console.log('OnBeforeMount: ButtonCounter.vue');
+});
+onMounted(() => {
+  console.log('OnMounted: ButtonCounter.vue');
+});
+onBeforeUnmount(() => {
+  console.log('OnBeforeUnmount: ButtonCounter.vue');
+});
+onUnmounted(() => {
+  console.log('OnUnmounted: ButtonCounter.vue');
+});
+
+const count = ref(0);
+
+</script>
