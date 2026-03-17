c5b10b054ace0895241e891ea513dbfc7bcb61f9
Morgan <ycshinichi@163.com>
2026-03-17 08:39:50 +0800

    slots

---
+++ Section 13 Slots and Life Cycle/slot/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <luckyNumberParentComponent>
+    <p class="pt-2">We have two versions for picking lucky numbers!</p>
+    <p>Click the button to toggle between those two versions.</p>
+    <hr/>
+  </luckyNumberParentComponent>
+</template>
+<script setup>
+import LuckyNumberParentComponent from './components/LuckyNumberParentComponent.vue';
+</script>
---
+++ Section 13 Slots and Life Cycle/slot/src/components/LuckyNumberParentComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container bg-black text pt-3" :style="{height: '100vh'}">
+      <h1 class="text-center text-success">Learn Slots</h1>
+  
+      <div class="row align-items-center">
+        <slot></slot>
+        <button class="btn btn-primary text-black m-2 col-6" @click="toggleComponent">
+          Toggle Components
+        </button>
+        <br/>
+          <button class="btn btn-primary text-black m-2 col-6" @click="showLuckyNumber">
+            Lucky Number1
+          </button>
+          <button class="btn btn-primary text-black m-2 col-6" @click="showLuckyNumberV2">
+            Lucky Number2
+          </button>
+        <br/>
+        <keep-alive :include="['LuckyNumberV2', 'LuckyNumber']">
+          <component :is="currentComponent" class="border"/>
+        </keep-alive>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script setup>
+  import { ref, reactive,provide,compile,computed } from 'vue';
+import LuckyNumber from './LuckyNumber.vue';
+import LuckyNumberV2 from './LuckyNumberV2.vue';
+  
+  const newVersion = ref(true);
+  const currentComponent = computed(() => {
+    return newVersion.value ? LuckyNumberV2 : LuckyNumber;
+  });
+  
+    // Method to toggle component
+    function toggleComponent() {
+      newVersion.value = !newVersion.value;
+    }
+  
+    // Show LuckyNumber component
+    function showLuckyNumber() {
+      newVersion.value = false;
+    }
+  
+    // Show LuckyNumberV2 component
+    function showLuckyNumberV2() {
+      newVersion.value = true;
+    }
+  
+  
+  
+  
+  
+  </script>
