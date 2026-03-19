b2671e299792515b66becf38ac2ac7f3062f7bf7
Morgan <ycshinichi@163.com>
2026-03-19 12:33:15 +0800

    props in composibles

---
+++ Section 13 Slots and Life Cycle/props in composibles/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+  <p>Count: {{ count }}</p>
+  <br>
+  <ButtonCounter></ButtonCounter>
+</template>
+<script setup>
+import { useCounter } from './composibles/useCounter';
+import ButtonCounter from './components/ButtonCounter.vue';
+const { count, increment, decrement } = useCounter(100,10);
+</script>
---
+++ Section 13 Slots and Life Cycle/props in composibles/src/components/ButtonCounter.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="increment">++</button>
+  <span>Count: {{ count }}</span>
+  <button @click="decrement">--</button>
+  
+</template>
+<script setup>
+  import { useCounter } from '../composibles/useCounter';
+const { count, increment, decrement } = useCounter();
+</script>
