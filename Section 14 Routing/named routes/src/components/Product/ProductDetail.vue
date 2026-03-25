0a019097159adb7be512935c044b0fecc7adf056
Morgan <ycshinichi@163.com>
2026-03-25 12:35:37 +0800

    named routes

---
+++ Section 14 Routing/named routes/src/components/Product/ProductDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Product Detail</h1>
+    <div v-if="resolvedProductId">
+      <p>Product ID: {{ resolvedProductId }}</p>
+    </div>
+    <div v-else>
+      <p>No product Id found</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const props = defineProps<{
+  productId?: string
+}>()
+
+const route = useRoute()
+const resolvedProductId = computed(
+  () => props.productId ?? (route.params.productId as string | undefined),
+)
+</script>
